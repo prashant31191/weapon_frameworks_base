c84b05aa2b75d597e18f3cb82598dbac100f52cd
soupy031 <jasonsurprenant@yahoo.com>
2014-01-27 14:52:13 -0500

    just a small clean-up

--- packages/SystemUI/src/com/android/systemui/quicksettings/ContactTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/ContactTile.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2013 The SlimRoms Project
+ * Copyright (C) 2014 WeaponX ROM
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 
 package com.android.systemui.quicksettings;
 
-import static com.android.internal.util.slim.QSConstants.TILE_CUSTOM_KEY;
+import static com.android.internal.util.weaponx.QSConstants.TILE_CUSTOM_KEY;
 
 import android.content.ContentResolver;
 import android.content.Context;
